0dc6ed67f670a4ebc9e4150abc00e1327245d7cb
Le <Kenry_Le1@baylor.edu>
2020-10-15 09:39:29 -0500

    fifth commit

--- settings.py
+++ settings.py
@@ -22,6 +22,6 @@ class Settings:
         self.alien_speed = 1.0
         self.fleet_drop_speed = 10
         # fleet-direction of 1 represents right; -1 represents left.
-        self.fleet_direction = 1
+        self.fleet_direction = 1 
 
         
